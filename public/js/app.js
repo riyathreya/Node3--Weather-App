06d8204e90caabff36ddfac1d1b8b49015b3aa5f
riya <riyathreya@gmail.com>
2021-12-04 00:11:30 +0530

    Initial Commit

---
+++ public/js/app.js
@@ -0,0 +1,38 @@
+console.log("client side JS file is loaded");
+
+
+// fetch('https://puzzle.mead.io/puzzle').then((response) => {
+//     response.json().then((data)=> {
+//         console.log(data)
+//     })
+// })
+
+
+
+
+const weatherFrom = document.querySelector('form')
+const search = document.querySelector('input')
+
+const message1 = document.querySelector('#message-1')
+const message2 = document.querySelector('#message-2')
+
+
+
+
+weatherFrom.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const location = search.value;
+    //console.log(location);
+    const url = 'http://localhost:3000/weather?address=' + location;
+    fetch(url).then((response) => {
+        response.json().then((data) => {
+            if(data.error){
+                message1.textContent = JSON.stringify(data.error)
+                message2.textContent = ''
+            }
+            else {
+                message1.textContent = ''
+                message2.textContent =  JSON.stringify(data.forecast)  }
+        })
+    })
+})
---
+++ src/app.js
@@ -0,0 +1,113 @@
+const express = require('express');  //handlebars 
+const path = require('path');
+const hbs = require('hbs');
+const geocode = require('./utils/geocode');
+const forecast = require('./utils/forecast');
+
+
+const app = new express();
+
+//defined paths for express config
+const publicDirectoryPath = path.join(__dirname, '../public'); //files in public folder can be rendered directly (static files)
+const viewsPath = path.join(__dirname, '/templates/views');
+const partialsPath = path.join(__dirname,'/templates/partials'); 
+
+
+app.set('view engine', 'hbs'); //setting up handlebars  
+app.set('views', viewsPath);   //setting the path of default views folder to the new templates folder where handlbar templates are defined
+hbs.registerPartials(partialsPath);
+
+//setup static directory to serve
+app.use(express.static(publicDirectoryPath)); //static html content can be put in public
+
+
+app.get('', (req, res) => {
+    //res.render('index'); //name of template created in views folder  -- handlebars --> render dynamic pages 
+    res.render('index', {
+      title: 'WEATHER',
+      name: 'Riya Athreya'
+    })
+});
+
+// app.get('/help', (req,res) => {    //JSON RESPONSE
+//     res.send([{
+//         name: 'riya',
+//         age: 23
+//     }, 
+//     {
+//         name: 'sarah'
+//     }])
+// })
+
+app.get('/about', (req,res) => {     //STRING RESPONSE 
+   res.render('about', {
+    title: 'about page',
+    name: 'Riya Athreya'
+  });  
+})
+
+app.get('/weather', (req,res) => {
+    if(!req.query.address){
+      return res.send({error: "Please enter an address"})
+    }
+    
+    const address = req.query.address;
+    geocode(address, (error, {latitude, longitude, location} = {}) => {
+      if(error){
+        return res.send({error}) //shorthand sytax
+      }
+
+      forecast(latitude, longitude, (error, forecastData) => {
+          if(error){
+            return res.send({error: error})
+          }
+          res.send({
+            forecast: forecastData,
+            location,
+            address: req.query.address
+          })
+      })
+    });   
+})
+
+app.get('/help', (req,res) => {
+  res.render('help', {
+    title: 'help text', 
+    name: 'Riya Athreya'
+  })
+})
+
+
+app.get('/products', (req,res) => {
+  if(!req.query.search){
+      return res.send({error: 'You must provide a search term'})
+     
+  }
+  res.send({
+    products: []
+  })
+})
+
+
+
+
+
+//matching request like '/help/abc' or any other in /help path
+app.get('/help/*',(req,res) => {
+  res.render('404', {
+     title: 'The help page ',
+     name: 'riya'})
+})
+
+
+//match anything that is not matched for 404 not found
+app.get('*',(req,res) => {
+  res.render('404', { 
+    title: "Page",
+    name: 'riya athreya'})
+})
+
+
+app.listen(3000, () => {
+  console.log("server is up on port 3000...")
+})
