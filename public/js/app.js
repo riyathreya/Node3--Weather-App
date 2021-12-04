f1af58b3e06169babc19666f92dd997ceb0d9fcb
riya <riyathreya@gmail.com>
2021-12-04 14:34:12 +0530

    Setup app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -23,7 +23,7 @@ weatherFrom.addEventListener('submit', (e) => {
     e.preventDefault();
     const location = search.value;
     //console.log(location);
-    const url = 'http://localhost:3000/weather?address=' + location;
+    const url = '/weather?address=' + location;
     fetch(url).then((response) => {
         response.json().then((data) => {
             if(data.error){
